dcf49c69b519beb3709d95bba522c11b49be7456
Giles Knap <giles.knap@diamond.ac.uk>
2024-07-27 10:07:30 +0100

    add Name field to db page test

--- tests/test_system/test_pages.py
+++ tests/test_system/test_pages.py
@@ -2,9 +2,7 @@ import os
 from pprint import pprint
 
 from notion_client import Client
-from pydantic import TypeAdapter
 
-from notion_data.dynamic import dict_model_instance
 from notion_data.page import Page
 
 secret = os.getenv("NOTION_SECRET")
@@ -20,8 +18,12 @@ def test_db_page():
 
     page = Page(**page_json)
     properties = page.properties
+    # not allowed to set these - TODO wrapper should remove them from the model
     del properties.Created_time
     del properties.Created_by
+
+    rich_text = page.properties.Name.title[0].text
+    rich_text.content += " COPY. MADE by test_db_page()"
 
     # make a copy of the page
     result = notion.pages.create(
@@ -31,7 +33,7 @@ def test_db_page():
 
     # delete the page we just created
     new_page = Page(**result)
-    # notion.pages.update(page_id=new_page.id, archived=True)
+    notion.pages.update(page_id=new_page.id, archived=True)
 
 
 def test_plain_page():
@@ -40,7 +42,6 @@ def test_plain_page():
 
     page = Page(**page_json)
 
-    # properties = dict_model_instance("props", page.properties)
     rich_text = page.properties.title.title[0].text
     rich_text.content += " COPY. MADE by test_plain_page()"
 
